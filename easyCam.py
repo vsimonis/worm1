8022a1fa9d1c5964dd9fb00040826451b51bd3c1
= <=>
2014-01-20 23:05:43 +0000

    cam commit again for completeness

--- easyCam.py
+++ easyCam.py
@@ -1,28 +1,53 @@
 import picamera
 import time
-
+from datetime import datetime
 class easyCam:
     def __init__( self ):
-        self.videoRes = None    #(x, y) tuple
+        self.vidRes = None    #(x, y) tuple
         self.frameRate = None   #frames per second
-        self.captureFreq = None #images per minute
-        self.vidLength = None #expressed in minutes
+        self.capFreq = None #images per minute
+        self.vidLen = None #expressed in minutes
 
     ## Set parameters for video capture
     # Resolution
-    def setVideoRes( self, x, y ):
-        self.videoRes = ( x, y )
+    def setVidRes( self, x, y ):
+        self.vidRes = ( x, y )
+    
+    def setVidLen( self, vidLength ):
+        self.vidLen = vidLength
 
     # Frame Rate    
     def setFrameRate( self, fRate ):
         self.frameRate = fRate
+    
+    def setVidName( self, vidNm ):
+        self.vidName = vidNm
 
     ## Set parameters for query images sent to image processor
     # Capture frequency expressed in image queries per min
-    def setCaptureFreq( self, freq):
-        self.captureFreq = freq
+    def setCapFreq( self, freq):
+        self.capFreq = freq
     
-    def setVidLength
+   
+    ## Recording Methods    
+    def recordVid( self ):
+        with piCamera() as camera: 
+            if self.vidRes or self.vidLen or self.vidName == None:
+                print "Please set a resolution, video length or name"
+            
+            else :
+                camera.resolution = self.vidRes
+                camera.start_recording( '%s.h264' % self.vidName )
+                camera.wait_recording( self.vidLen * 60 )
+                camera.stop_recording()
+
+    def pingImg ( self ):
+        with piCamera() as camera:
+            camera.resolution = self.vidRes
+            camera.start_preview()
+            time.sleep(2)
+            camera.capture('%s.jpg' % str( datetime.now() ))
+
 
 
  
