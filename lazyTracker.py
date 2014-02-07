43824888b267d7d468861d48ab370b9920578503
= <=>
2014-02-07 04:27:21 +0000

    lazy Tracker close to done

--- lazyTracker.py
+++ lazyTracker.py
@@ -1,13 +1,22 @@
 import io
 import time
 import picamera
-from imgProc import imgProc
+#from imgProc import imgProc
 #from PIL import Image
 from skimage import io as skio
 from skimage import color
 import numpy as np
 import threading
 from easyEBB import easyEBB
+
+DURATION = 1000; #in ms
+STEPX = 1/100 #pixels per step
+STEPY = 1/100 #pixels per step
+BOUNDX = 200; #pixels
+BOUNDY = 200; #pixels
+VIDLEN = 20; #in sec
+PING = 1; #in sex
+
 
 def get_frame(stream):
     for frame in stream.frames:
@@ -33,26 +42,27 @@ def write_video( stream, img ):
         #img = Image.open(stream.read1(frame.frame_size)) 
         #x, y = imgProc.getCentroidFromRaw(img)
         #print "x: %d\ty: %d"%(x,y)
-
+def move(ebb, xx, yy):
+    ebb.stepM(DURATION, xx*STEPX, yy*STEPY)
 ### RUNNING PARTS
-ebb() = new 
+ebb = new easyEBB() 
 with picamera.PiCamera() as camera:
     stream = picamera.PiCameraCircularIO(camera, seconds = 20)
     stream2 = io.BytesIO()
     camera.start_recording(stream, format='h264')
     startT = time.time() # start time
-    lastCheck = startT - 1 # artificial last check
+    lastCheck = startT - PING # artificial last check
     now = startT
     ref = 0;
-    xds = np.array(10);
-    yds = np.array(10);
+    xds = np.array(WINDOW);
+    yds = np.array(WINDOW);
     xr = 0;
     yr = 0;
     try: 
-        while now - startT <= 20: #while less than video length
+        while now - startT <= VIDLEN: #while less than video length
             now = time.time()
             #camera.wait_recording(1)
-            if now - lastCheck >= 1:
+            if now - lastCheck >= PING:
                 lastCheck = time.time()
                 camera.capture(stream2, format='jpeg', use_video_port = True)
                 stream2.seek(0)
@@ -70,8 +80,8 @@ with picamera.PiCamera() as camera:
                         yds = yds[1:WINDOW-1]
                     mx = np.mean(xds)
                     my = np.mean(yds)
-                    if abs(mx) > 200 or abs(my) > 200:
-                        #move(mx, my)
+                    if abs(mx) > BOUNDX or abs(my) > BOUNDY:
+                        move(ebb, mx, my)
                         print mx
                         print my
                         ref = 0;
