3e2ec0c13b41af15fe939e27cccafc89fdd42e70
= <=>
2014-02-12 21:10:01 +0000

    true multithreaded example port issues

---
+++ expertTracker.py
@@ -0,0 +1,166 @@
+#!/usr/bin/python
+
+import threading #look Ma! I'm multi-threaded :)
+import time #for timeing and stuff
+import io #for streams
+from PIL import Image #for reading in images
+import numpy as np #for matrix maths
+import picamera #connexion to camera
+
+
+### Wouldn't be a Valerie program unless I had allllllll these globals :)
+## General
+V = True #If true, tons of print statements
+START = time.time()
+
+## Threading
+exitFlag = 0
+
+## Worm-finding
+WINDOW = 10
+VIDLEN = 30
+VIDNAME = 'mttest1.h264' 
+BOUNDROW = 200
+BOUNDCOL = 200
+
+'''
+Runs the worm thread
+'''
+class wormThread ( threading.Thread ):
+    def __init__( self, threadID, name, cameraObj ):
+        threading.Thread.__init__( self )
+        self.threadID = threadID
+        self.name = name
+        self.cameraObj = cameraObj
+
+    def run( self ):
+        print '%s\t%s\tStarting' % ( time.ctime(time.time()), self.name )
+        findWorm( self.name, self.cameraObj )
+        print '%s\t%s\tExiting' % ( time.ctime(time.time()), self.name )
+        
+
+class writeThread ( threading.Thread ):
+    def __init__( self, threadID, name, stream, delay ):
+        threading.Thread.__init__( self )
+        self.threadID = threadID
+        self.name = name
+        self.stream = stream
+        self.delay = delay
+
+    def run( self ):
+        print '%s\t%s\tStarting' % ( time.ctime(time.time()), self.name )
+        writeVid( self.name, self.stream, self.delay )
+        print '%s\t%s\tExiting' % ( time.ctime(time.time()), self.name )
+
+
+
+'''
+Finds the worm
+TODO: Optimization 
+TODO: sampling var, but realistically RPi won't be able to do this fast enough for us to specify this unless it's > 3 seconds
+'''
+
+def findWorm(threadName, camObj):
+    # Keep doing this until VIDEO is DONE
+    ref = None
+    while time.time() - START <= 20: #!EXITF :     
+        
+        if exitFlag:
+            thread.exit()
+        
+        # Capture image from cameraObj
+        picStream = io.BytesIO()
+        print '%s\t%s\tpicStream' % ( time.ctime( time.time() ), threadName )
+        camObj.capture( picStream, format = 'jpeg', use_video_port = True )
+        print '%s\tpast cam obj' % ( time.ctime( time.time() ), threadName )
+        stream.seek(0)
+        
+        if ref is None: #then camera has moved or we just got started
+            if V: "New Reference frame acquired"
+            ref = rgb2grayV( Image.open( stream ) ).astype(float) #get image, mk gray, as float
+        
+        else: #we already have a reference!
+            if V: "New Comparison frame acquired"
+            comp = rgb2grayV( Image.open( stream ) ).astype(float)
+            sub = comp - ref
+            
+            #Find the maximum gray scale values -- corresponds to COMPARISON WORM (note: returns an array)
+            col, row = np.nonzero( sub == sub.max() )
+            
+            # Find the minimum gray scale values -- corresponds to REFERENCE WORM
+            if colr is None: #only process the reference image first time 'round
+                if V: print 'Getting Maximum'
+                colr, rowr = np.nonzero( sub == sub.min() )
+
+            # For 'windowing' only consider a window of distances when making decision about whether or not to move
+                #smoothes the location of the worm 
+            coldist.append(colr[0] - col[0]) #arrays cuz np.nonzero returns all indices of maxima/minima in image
+            rowdist.append(rowr[0] - row[0])
+            
+            if len(coldist) > WINDOW:
+                #coldist.pop(0)
+                #rowdist.pop(0)
+                coldist = coldist[1:]
+                rowdist = rowdist[1:]
+            
+            mcol = np.mean(coldist)
+            mrow = np.mean(rowdist)
+            
+            if abs(mrow) > BOUNDROW or abs(mcol) > BOUNDCOL:
+                print 'Move! row: %d \t col: %d' % ( rowdist[ len( rowdist ) - 1 ] , coldist [ len( coldist ) - 1 ] ) 
+                ref = None; #triggers getting new reference image
+                colr = None; #triggers getting new reference row/col position
+            
+                
+
+
+def writeVid(threadName, stream, delay):
+    while time.time() - START <= VIDLEN:
+        print '%s\t%s\twriting vid' % ( time.ctime( time.time() ), threadName )
+        with stream.lock:
+            print '%s\t%s\tpast stream.lock' % ( time.ctime( time.time() ), threadName )
+            for frame in stream.frames:
+                print '%s\t%s\tframe' % ( time.ctime( time.time() ), threadName )
+                if frame.header:
+                    print '%s\t%s\tframe.header' % ( time.ctime( time.time() ), threadName )
+                    stream.seek(frame.position)
+                    print '%s\t%s\tpast stream seek' % ( time.ctime( time.time() ), threadName )
+                    break
+
+        with io.open(VIDNAME, 'wb') as output:
+            print '%s\t%s\t past io.open' % threadName
+            output.write( stream.read() )
+            print '%s\t%s\tpast io write' % ( time.ctime( time.time() ), threadName )
+        
+        print '%s\t%s\tsleeping'  % ( time.ctime( time.time() ), threadName )
+        time.sleep(delay)
+               
+    exitFlag = 1
+
+
+### Helper helpers
+def rgb2grayV(I):
+    I = I.astype( float )
+    return 1.0/3 * ( I[:,:,0] + I[:,:,1] + I[:,:,2] )
+    
+
+
+### Actual main method
+
+with picamera.PiCamera() as camera:
+    camera.resolution = (1080, 1080)
+    camera.framerate = 25
+
+    stream = picamera.PiCameraCircularIO( camera, seconds = 10 )
+    camera.start_preview()
+    
+    print 'start recording'
+    camera.start_recording( stream, format = 'h264' )
+
+    wormT = wormThread(1, 'wormworm',  camera ) 
+    writeT = writeThread(2, 'writewrite', stream, 5)
+
+    wormT.start()
+    writeT.start()
+    
+    camera.stop_recording()
--- zTrack.py
+++ zTrack.py
@@ -5,12 +5,14 @@ Created on Feb 11, 2014
 '''
 
 import io
-import camera
+import picamera
 import numpy as np
 from PIL import Image
 import time
 from easyEBB import easyEBB
-
+import random #testing
+import matplotlib.pyplot as plt
+import thread
 
 #### Method definitions
 def rgb2grayV(I):
@@ -26,6 +28,7 @@ def rgb2grayV(I):
     
 
 def findWorm(camera):
+
     global ref
     global xr, yr
     global xds, yds
@@ -36,27 +39,38 @@ def findWorm(camera):
     stream.seek(0)
     if ref is None:
         if V: print 'new reference'
-        ref = rgb2grayV(Image.open(stream)).astype(float)
+        #ref = rgb2grayV(Image.open(stream)).astype(float)
+        ref = rgb2grayV(np.asarray(Image.open(stream)).astype(float))
+        #print ref
+        
     else:
         if V: print 'new current'
-        current = rgb2grayV(Image.open(stream)).astype(float)
+        #current = rgb2grayV(Image.open(stream)).astype(float)
+        current = rgb2grayV(np.asarray(Image.open(stream)).astype(float))
         sub  = current - ref
-        if V: print 'get max'
-        x, y = np.nonzero(sub == sub.max())
+        if V and V1: print 'get max'
+        x, y = np.nonzero(sub == np.max(sub))
         if xr is None: # Only get refernce coordinates if the reference is new
-            if V: print 'get min'
-            xr, yr = np.nonzero(sub == sub.min())
+            if V and V1: print 'REFERENCE coords'
+            xr, yr = np.nonzero(sub == np.min(sub))
+        if P:
+            camera.stop_preview()
+            plt.imshow(sub, cmap = 'gray')
+            plt.scatter(y, x, c = 'r')
+            plt.scatter(yr, xr, c = 'b')
+            plt.show()
+
         # add distances to decision vector for average distance from ref
-        if V: print 'append dists'
+        if V and V1: print 'append dists'
         xds.append(x[0] - xr[0])
         yds.append(y[0] - yr[0])
         
         if len(xds) > WINDOW:
-            if V: print 'pop dists'
+            if V and V1: print 'pop dists'
             xds.pop()
             yds.pop()
         
-        if V: print 'mean dists'
+        if V and V1: print 'mean dists'
         mx = np.mean(xds)
         yx = np.mean(yds)
         
@@ -64,28 +78,33 @@ def findWorm(camera):
             print 'move'
             ref = None #will force a new reference image
             xr = None #will force a new reference coordinate
-        
-        return True
+ 
+    return True
+#return random.randint(0,10) == 0 #True
 
 def write_vid(stream):
     with stream.lock:
-        with io.open('dpu-test1.h264', 'wb') as out:
-            for frame in stream.frames:
-                if frame.header:
-                    stream.seek(frame.position)
-                    break
-            while True:
-                buf = stream.read1()
-                if not buf:
-                    break
-                out.write(buf)
-        stream.seek(0)
-        stream.truncate()
+        for frame in stream.frames:
+            if frame.header:
+                stream.seek(frame.position)
+                break
+        
+        with io.open('test1.h264', 'wb') as out:
+            #while True:
+            #buf = stream.read1()
+            #if not buf:
+                #break
+            #out.write(buf)
+            out.write(stream.read())
+    #stream.seek(0)
+    #stream.truncate()
     if V: print 'wrote vid'
 
 #### PORTION THAT RUNS
 ## Verbosity vars
 V = True
+V1 = False
+P = True
 
 ## Image subtraction vars
 ref = None 
@@ -114,13 +133,19 @@ ebb = easyEBB()
 with picamera.PiCamera() as camera:
     camera.resolution = RES
     camera.framerate = FR
+    camera.start_preview()
     stream = picamera.PiCameraCircularIO(camera, seconds = 10)
     camera.start_recording(stream, format = 'h264')
     
-    try:
+    try:     
         now = time.time()
-        while(start - now <= VIDLEN):
+        while(now - start <= VIDLEN):
+            now = time.time()
+            print now - start
+            #a = thread.start_new_thread(findWorm, camera)
             if findWorm(camera):
+                #camera.wait_recording(5)
+                #thread.start_new_thread(write_vid,stream)
                 write_vid(stream)
     finally:
         camera.stop_recording()
